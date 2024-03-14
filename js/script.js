8ffe26b75b9999370f2cec30d8479537cd11d027
Aligern <sasso.luca93@gmail.com>
2024-03-14 13:04:28 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// We ask the user to give us a word then we need to create a function to see if it is palindrome
+
+// then the user choose odd or even and give us a number between 1 and 5.
+// we generate a random number (always between 1 a 5) with another function.
+// we sum the two numbers
+// we need to check if the sum is odd or even with a function.
+// then we print who the winner is.
